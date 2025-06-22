f4922a2b88aaaf62574c083c545c44ba3164999b
Damian Portilla <damianportilla59@gmail.com>
2025-06-22 02:58:24 +0000

    ya funciona ahora si agreaga funcionalidades

--- src/components/facturas/factura-form-modal.tsx
+++ src/components/facturas/factura-form-modal.tsx
@@ -1,6 +1,7 @@
+
 "use client";
 
-import { useEffect, useActionState, useTransition, useState } from "react";
+import { useEffect, useTransition, useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type { z } from "zod";
